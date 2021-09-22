511f76e3005c21d6b9b51df2470d4fecce369a8b
fajarnurwahid <fajarnurwahid12@gmail.com>
2021-09-22 15:02:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const input = document.querySelector('.tags .input-tag');
+const tagWrapper = document.querySelector('.tags');
+const removeAll = document.querySelector('.bottom .remove-all');
+const totalTag = document.querySelector('.bottom p .total-tag');
+let listTag = [];
+
+
+totalTag.textContent = listTag.length;
+
+input.addEventListener('input', function () {
+	if(this.value.includes(',')) {
+		const str = this.value.toLowerCase().replace(/ +/g, ' ').split(',');
+		str.forEach(item=> {
+			const text = item.trim();
+			if(!listTag.includes(text) && text !== "" && text !== " ") {
+				listTag.push(text);
+				addTag();
+			}
+		})
+		totalTag.textContent = listTag.length;
+		this.value = "";
+	}
+})
+
+
+
+function addTag() {
+	tagWrapper.querySelectorAll('li').forEach(li=> li.remove())
+	listTag.forEach(item=> {
+		const li = document.createElement('li');
+		li.innerHTML = `
+			<span class="text">${item}</span>
+			<i class='bx bx-x close-btn' ></i>
+		`
+		tagWrapper.insertBefore(li, input)
+	})
+}
+
+
+
+window.addEventListener('click', function (e) {
+	if(e.target.matches('.bx.bx-x.close-btn')) {
+		const li = e.target.parentElement
+		const text = li.querySelector('.text').textContent
+		tagWrapper.removeChild(li)
+		listTag.splice(listTag.indexOf(text), 1);
+		totalTag.textContent = listTag.length;
+	}
+})
+
+removeAll.addEventListener('click', function() {
+	tagWrapper.querySelectorAll('li').forEach(li=> li.remove())
+	listTag = [];
+	totalTag.textContent = listTag.length;
+})
